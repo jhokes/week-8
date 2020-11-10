c68950bbeb74df60b697382592daa831c20365ea
Jala Hokes <Jala@MacBook-Pro.local>
2020-11-09 23:00:17 -0500

    first commit

---
+++ Easy/script.js
@@ -0,0 +1,12 @@
+var names = []
+names[0] = 'Torren Spann-Bell'
+names[1] = 'Jordan McKoy'
+names[2] = 'Jala Hokes'
+
+
+var ages = []
+ages[0] = 20
+ages[1] = 22
+ages[2] = 19
+
+console.log('Hello my name is' + " " + names[2] + ' ' + 'and I am' + " " + ages[2] + " " + 'years old')
